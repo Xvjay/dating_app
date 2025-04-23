745c536a2c9e4079e79b5452429012154b82f1b3
Xvjay <Chrisjay12390@gmail.com>
2025-04-23 17:22:21 -0400

    register page now has text boxes

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'componets/text_box.dart';
+import 'componets/nav_button.dart';
+import 'main.dart';
 
 class SecondRoute extends StatelessWidget {
   const SecondRoute({super.key});
@@ -6,23 +9,30 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-        centerTitle: true,
-        backgroundColor: const Color(
-          0xFFFFB6C1,
-        ), // Matching the AppBar color from the main screen
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Welcome to the Registration Screen',
-              style: TextStyle(fontSize: 20),
-            ),
-            // You can add your registration form or other widgets here.
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 1),
+            colors: <Color>[
+              Color(0xFFFF5858), // This is the base pinkish tone
+              Color(0xFFFFC8C8),
+            ], //
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CustomBox(labelText: "Enter Username"),
+              CustomBox(labelText: "Enter Password"),
+              CustomBox(labelText: "Re-Enter Password"),
+              CustomBox(labelText: "Enter Female or Male"),
+              NavButton(destination: MyHomePage(), name: "register"),
+
+              // Add more text boxes or widgets as needed
+            ],
+          ),
         ),
       ),
     );
